cd253bd42c68a51438b0bfcf2634ae368bcfe9c2
ngocphan90 <bichngoc0803@yahoo.com>
2021-10-18 04:41:22 -0500

    dynamic built in array

--- src/com/codewithmosh/Array.java
+++ src/com/codewithmosh/Array.java
@@ -15,15 +15,33 @@ public class Array {
             //create a new array (twice the size)
             //copy all the existing items
             int[] newItems = new int [count*2];
-            for (int i =0; i< count; i++){
+            for (int i =0; i< count; i++) {
                 newItems[i] = items[i];
+            }
             //set "items" to this new array
             items = newItems;
-            }
-        }
 
+        }
         //add new item at the end of the array
         items[count++] = item;
+    }
+    public void removeAt(int index){
+        if (index<0|| index>=count){
+            throw new IllegalArgumentException();
+        }
+
+        for (int i =index; i< count; i++) {
+            items[i] = items[i + 1];
+        }
+        //count present number of item in the array not the size of the array
+        count--;
+    }
+    // O(n) in worst case
+    public int indexOf( int item){
+        for (int i =0 ; i< count; i++)
+            if (items[i] == item)
+                return i;
+        return -1;
     }
 
     public void print (){
--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,6 +1,9 @@
 package com.codewithmosh;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,8 +11,18 @@ public class Main {
         Array numbers = new Array(1);
 
         numbers.insert(20);
-        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.removeAt(0);
         numbers.print();
+
+        //built in dynamic array has built in add, remove  and indexOf function
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(200);
+        list.remove(0);
+        list.indexOf(0);
+        System.out.println(list);
 
 
     }
